ea19f7e879325c925ca60504ff21a0f8f86e2b98
PuffyNutty <sewerynpatrick@gmail.com>
2026-06-02 10:47:41 +0930

    Project Architecture

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
-import ("fmt")
+import (
+    "fmt"
+    "os"
+)
+
+
+
+
+type FilePath = string
+
+
+
+
+func listItemSymlinks(item FilePath) {
+    
+}
+
+
+
+
+func parseArguments(arg string) {
+
+    switch arg {
+    case "list":
+	var item: FilePath = os.Args[2]
+	return listItemSymlinks(item)
+
+    case "move":
+	var item FilePath = os.Args[2]
+	var destination FilePath = os.Args[3]
+	return moveItemToNewDir(item, destination)
+
+    case "delete":
+	var item FilePath = os.Args[2]
+	return deleteItem(item)
+
+    case "search":
+	var directory = os.Args[2]
+	return searchDirForBrokenSymlinks(directory)
+
+    default:
+	return false
+    }
+}
+
+
+
+
+func parseArguments() {
+}
+
+
 
 
 func main() {
-    fmt.Println("Hello world\n");
+    argCount := len(os.Args)
+    const firstArgIdx = 1
+
+    
+    for i := firstArgIdx; i < argCount; i++ {
+	fmt.Println(i)
+    }
+    
 }
