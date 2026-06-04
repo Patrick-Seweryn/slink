69bb5a6bf2a9f0a0e65f0c31b16179cb0e2a140d
PuffyNutty <sewerynpatrick@gmail.com>
2026-06-04 12:46:23 +0930

    Beautiful handling of function arguments

--- slink/main.go
+++ slink/main.go
@@ -4,14 +4,5 @@ package main
 
 
 func main() {
-    parseArguments()
-/*
-    argCount := len(os.Args)
-    const firstArgIdx = 1
-
-    
-    for i := firstArgIdx; i < argCount; i++ {
-	fmt.Println(i)
-    }
- */   
+    computeExecutable()
 }
